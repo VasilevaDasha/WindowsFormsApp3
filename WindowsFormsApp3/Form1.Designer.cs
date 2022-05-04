493b89265103cbe075c398b6952e9e2df72d7f7a
Дарья <@DESKTOP-NQCLB6J>
2022-05-04 11:45:29 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MouseDoubleClick = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MouseDoubleClick)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MouseDoubleClick
+            // 
+            this.MouseDoubleClick.Image = global::WindowsFormsApp3.Properties.Resources.mouse1;
+            this.MouseDoubleClick.Location = new System.Drawing.Point(146, 38);
+            this.MouseDoubleClick.Name = "MouseDoubleClick";
+            this.MouseDoubleClick.Size = new System.Drawing.Size(260, 260);
+            this.MouseDoubleClick.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.MouseDoubleClick.TabIndex = 0;
+            this.MouseDoubleClick.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MouseDoubleClick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.MouseDoubleClick)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        PictureBox MouseDoubleClick;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MouseDoubleClick.MouseEnter += new EventHandler(this.MouseDoubleClick_MouseEnter);
+            MouseDoubleClick.MouseLeave += new EventHandler(this.MouseDoubleClick_MouseLeave);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x_mouse = e.X;
+            int y_mouse = e.Y;
+            MessageBox.Show("Координаты мыши внутри Form" + x_mouse + " " + y_mouse);
+        }
+        private void MouseDoubleClick_MouseEnter(object sender, EventArgs e)
+        {
+            // MouseDoubleClick.BackColor = Color.CadetBlue;
+            MouseDoubleClick.Image = Image.FromFile("c:\\mouse2.png");
+            MouseDoubleClick.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+        private void MouseDoubleClick_MouseLeave(object sender, EventArgs e)
+        {
+            // MouseDoubleClick.BackColor = Color.AliceBlue;
+            MouseDoubleClick.Image = Image.FromFile("c:\\mouse1.png");
+            MouseDoubleClick.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
